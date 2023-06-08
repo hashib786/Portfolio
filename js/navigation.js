f25ff9ea7256e93ca2c22873b5a7445ec5253ffd
Hashib Raja <hashibraja23@gmail.com>
2023-06-08 19:30:02 +0530

    completed Responsiveness and also added feature of hemburger

--- js/navigation.js
+++ js/navigation.js
@@ -1,3 +1,50 @@
-const sections = [...document.getElementsByClassName(".section")];
+let sections = [...document.getElementsByClassName("section")];
+const navLinks = [...document.querySelectorAll(".nav__links ul li")];
+const moreAbout = document.getElementById("moreAbout");
+const hireMe = document.getElementById("hireMe");
 
-export const navigation = () => {};
+const navContainer = document.querySelector(".nav__container");
+const blurArea = document.querySelector(".blur__effect");
+const hemburgers = document.querySelector(".hemburgers");
+const hemburger = document.querySelector(".hemburger");
+const cross = document.querySelector(".cross");
+
+export const navigation = () => {
+  const render = (index) => {
+    navLinks.forEach((ele) => ele.classList.remove("active"));
+    sections[index].classList.add("active");
+    navLinks[index].classList.add("active");
+    setTimeout(
+      (index) => {
+        sections.forEach((ele, number) => {
+          if (number === index) return;
+          ele.classList.remove("active");
+        });
+      },
+      0,
+      index
+    );
+  };
+
+  navLinks.forEach((ele, index) => {
+    ele.addEventListener("click", () => {
+      render(index);
+    });
+  });
+
+  moreAbout.addEventListener("click", () => render(1));
+  hireMe.addEventListener("click", () => render(4));
+
+  const closeHemburger = () => {
+    hemburgers.classList.remove("active");
+    navContainer.classList.remove("active");
+  };
+
+  hemburger.addEventListener("click", () => {
+    hemburgers.classList.add("active");
+    navContainer.classList.add("active");
+  });
+
+  cross.addEventListener("click", closeHemburger);
+  blurArea.addEventListener("click", closeHemburger);
+};
