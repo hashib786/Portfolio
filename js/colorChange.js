858d42b4d06770a322fa14bec5c5a956ca7cb489
Hashib Raja <hashibraja23@gmail.com>
2023-07-31 20:17:54 +0530

    remove multiple eventlisner for color changing added in parent only eventlisner

--- js/colorChange.js
+++ js/colorChange.js
@@ -2,6 +2,8 @@ const settingBtn = document.querySelector(".setting");
 const backgroundChange = document.querySelector(".backgroundChange");
 const essentialContainer = document.querySelector(".essential__container");
 const colorItem = [...document.getElementsByClassName("colors__item")];
+const workingColor = document.querySelector(".working__colors");
+console.dir(workingColor);
 
 // checking default background
 const { matches } = window.matchMedia("(prefers-color-scheme: dark)");
@@ -23,16 +25,16 @@ export const colorChange = () => {
     );
   });
 
-  colorItem.forEach((ele, index) => {
-    ele.addEventListener("click", (e) => {
-      e.stopPropagation();
-      const classVal = index + 1;
-      if (classVal > 5 && classVal < 1) return;
-      const classes = [...document.body.classList].filter(
-        (ele) => !ele.includes("color")
-      );
-      document.body.classList = `${classes.join(" ")} color${classVal}`;
-    });
+  workingColor.addEventListener("click", (e) => {
+    e.stopPropagation();
+    const childColor = e.target.closest(".colors__item");
+    if (!childColor) return;
+    const classVal = parseInt(childColor.getAttribute("data-colorId"));
+    if (isNaN(classVal) || (classVal > 5 && classVal < 1)) return;
+    const classes = [...document.body.classList].filter(
+      (ele) => !ele.includes("color")
+    );
+    document.body.classList = `${classes.join(" ")} color${classVal}`;
   });
 
   backgroundChange.addEventListener("click", () => {
