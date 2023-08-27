32d58b13ff988c62a18195ad2a186e7c4987ff81
Hashib Raja <hashibraja23@gmail.com>
2023-08-27 14:10:53 +0530

    implemented for sending mail in our app

--- js/sendMail.js
+++ js/sendMail.js
@@ -1,5 +1,38 @@
 export const sendMail = () => {
   const mailForm = document.getElementById("sendMailForm");
 
-  mailForm.addEventListener("submit", () => {});
+  mailForm.addEventListener("submit", async (e) => {
+    e.preventDefault();
+
+    const name = document.getElementById("name");
+    const email = document.getElementById("email");
+    const subject = document.getElementById("subject");
+    const message = document.getElementById("message");
+
+    const response = await fetch(
+      "https://send-mail-hashib.onrender.com/sendMail",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          email: email.value,
+          subject: subject.value,
+          name: name.value,
+          message: message.value,
+        }),
+      }
+    );
+
+    if (response.ok) {
+      const newPost = await response.json();
+      name.textContent = "";
+      email.textContent = "";
+      subject.textContent = "";
+      message.textContent = "";
+    } else {
+      console.error("Error creating post:", response.statusText);
+    }
+  });
 };
